5d6fce73930e4acdebf611a2672c0f300d5256a4
Kayto <122955255+KaytoJoestarr@users.noreply.github.com>
2023-02-02 01:11:55 -0700

    Update ExampleInstrumentedTest.kt

--- guessthenumber/app/src/androidTest/java/flores/elizabeth/guessthenumber/ExampleInstrumentedTest.kt
+++ guessthenumber/app/src/androidTest/java/flores/elizabeth/guessthenumber/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package encinas.maria.guessthenumber
+package flores.elizabeth.guessthenumber
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("encinas.maria.guessthenumber", appContext.packageName)
+        assertEquals("flores.elizabeth.guessthenumber", appContext.packageName)
     }
 }
